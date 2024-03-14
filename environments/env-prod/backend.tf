49ba7aaa362bd6015fb2b17bd1a765621cd71378
janmejai singh <janmejaisinghg@gmail.com>
2024-03-15 01:47:54 +0530

    backend.tf file added

---
+++ environments/env-prod/backend.tf
@@ -0,0 +1,12 @@
+/*
+
+terraform {
+  backend "s3" {
+    bucket         = "<your_bucket_name>"
+    key            = "terraform.tfstate"
+    region         = "<your_aws_region>"
+    dynamodb_table = "<your_dynamo_dbtable_name>"
+  }
+}
+
+*/
